d4d4459339f6fa33bf16583dd98988b3eb5be27d
grynhas <patrick.d.s.reis@gmail.com>
2022-06-29 16:21:26 -0300

    modal implementation and basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import { GlobalStyles } from "./styles/global";
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
+import Modal from "react-modal";
+import { useState } from "react";
+
+import { GlobalStyles } from "./styles/global";
+
+Modal.setAppElement("#root");
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+  
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
+
+      <Modal 
+      isOpen={isNewTransactionModalOpen} 
+      onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h1>cadastrar</h1>
+      </Modal>
       <GlobalStyles />
     </>
   );
