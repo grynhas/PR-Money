d94a31261f6d7a946f5c4c3e4a9517763b6c0640
grynhas <patrick.d.s.reis@gmail.com>
2022-06-29 12:02:46 -0300

    Header componet styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { GlobalStyles } from "./styles/global";
+import { Header } from "./components/Header";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>hello word</h1>
+    <>
+      <Header />
       <GlobalStyles />
-    </div>
+    </>
   );
 }
